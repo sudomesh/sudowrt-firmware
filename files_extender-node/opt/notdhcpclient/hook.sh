9c5135481ee651a15aa5f9651e3361e82afa5a4e
Marc Juul <juul@sudomesh.org>
2015-07-08 05:33:06 -0700

    getting extender nodes ready to extend private network

--- files_extender-node/opt/notdhcpclient/hook.sh
+++ files_extender-node/opt/notdhcpclient/hook.sh
@@ -20,12 +20,20 @@ MESH_ETH="eth0.${MESH_VLAN}"
 OPEN_VLAN="10"
 OPEN_ETH="eth0.${OPEN_VLAN}"
 
+# The VLAN and interface that carries the private network
+PRIV_VLAN="11"
+PRIV_ETH="eth0.${PRIV_VLAN}"
+
 # The wireless interfaces
-MESH_WLAN="adhoc0"
+MESH_WLAN="mesh0"
 OPEN_WLAN="open0"
+PRIV_WLAN="priv0"
 
 # The bridge for the open (SSID: peoplesopen.net) network
 OPEN_BRIDGE="br-open"
+
+# The bridge for the private network
+PRIV_BRIDGE="br-priv"
 
 # Takes an IP string as an argument
 # returns success (0) if the IP is bad
@@ -89,6 +97,7 @@ case $STATE in
         echo "Creating VLAN interfaces"
         ip link add link $IFACE name $MESH_ETH type vlan id $MESH_VLAN
         ip link add link $IFACE name $OPEN_ETH type vlan id $OPEN_VLAN
+        ip link add link $IFACE name $PRIV_ETH type vlan id $PRIV_VLAN
 
         echo "Enabling wireless"
         # This does not persist between reboots
@@ -105,9 +114,10 @@ case $STATE in
         echo "Setting VLAN interface link states to up"
         ip link set dev $MESH_ETH up
         ip link set dev $OPEN_ETH up
+        ip link set dev $PRIV_ETH up
 
         # Configure and start babeld    
-        echo "Starting babeld"                                                     
+        echo "Starting babeld"
         uci set babeld.lan.ifname="$MESH_ETH"
         /etc/init.d/babeld start
 
@@ -117,31 +127,40 @@ case $STATE in
         brctl addif $OPEN_BRIDGE $OPEN_ETH
         brctl addif $OPEN_BRIDGE $OPEN_WLAN
 
+        # Create the private bridge between ethernet and wifi               
+        echo "Creating bridge between $PRIV_ETH and $PRIV_WLAN"
+        brctl addbr $PRIV_BRIDGE
+        brctl addif $PRIV_BRIDGE $PRIV_ETH
+        brctl addif $PRIV_BRIDGE $PRIV_WLAN
+
         echo "Assigning IP ${IP}/${NETMASK} to $OPEN_BRIDGE"
         ip addr add ${IP}/${NETMASK} dev $OPEN_BRIDGE
 
-#        echo "Setting route for $OPEN_BRIDGE"
-#        ip route add ${IP}/${NETMASK} dev $OPEN_BRIDGE proto kernel scope link
-
         echo "Setting $OPEN_BRIDGE state to up"
         ip link set dev $OPEN_BRIDGE up
+
+        echo "Setting $PRIV_BRIDGE state to up"                             
+        ip link set dev $PRIV_BRIDGE up
         ;;
 
 
     "down")
-        echo "Remove policy routing rules"
-        ip route del default dev $MESH_ETH table mesh_wlan
-        ip rule del iif $MESH_WLAN lookup mesh_wlan
-        ip route del default dev $MESH_WLAN table mesh_eth
-        ip rule del iif $MESH_ETH lookup mesh_eth
 
-        echo "Taking down bridge"
+        echo "Taking down bridge $OPEN_BRIDGE"
         ip link set dev $OPEN_BRIDGE down
 
-        echo "Removing bridge"
+        echo "Taking down bridge $PRIV_BRIDGE"
+        ip link set dev $PRIV_BRIDGE down
+
+        echo "Removing bridge $OPEN_BRIDGE"
         brctl delif $OPEN_BRIDGE $OPEN_ETH
         brctl delif $OPEN_BRIDGE $OPEN_WLAN
         brctl delbr $OPEN_BRIDGE
+
+        echo "Removing bridge $PRIV_BRIDGE"
+        brctl delif $PRIV_BRIDGE $PRIV_ETH
+        brctl delif $PRIV_BRIDGE $PRIV_WLAN
+        brctl delbr $PRIV_BRIDGE
 
         echo "Stopping babeld"
         /etc/init.d/babeld stop
@@ -153,6 +172,7 @@ case $STATE in
         echo "Removing VLAN interfaces"
         ip link del dev $MESH_ETH
         ip link del dev $OPEN_ETH
+        ip link del dev $PRIV_ETH
         ;;
 
     *)
