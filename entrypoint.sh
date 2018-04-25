1c9eb1cf11601bb503bd2839e681fd69a61576dc
paidforby <ggallo102@gmail.com>
2018-04-24 23:01:50 -0700

    preparing to merge back into master

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ARCH=ar71xx
 time ./build_only $ARCH
-# time ./build_extender-node $ARCH
+time ./build_extender-node $ARCH
 
 mkdir -p ./firmware_images
 cp -r ./built_firmware /firmware_images
