6e495f59a062dfd31ffba5d2fe5cd977327392c9
paidforby <ggallo102@gmail.com>
2018-05-15 20:48:06 +0000

    create create_build_machine script related to #111

---
+++ create_build_machine.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -x
+set -e
+
+SUDOWRT_REPO=sudomesh/sudowrt-firmware
+SUDOWRT_DIR=/opt/sudowrt-firmware
+CRON_TIME="00 00"
+
+DEBIAN_FRONTEND=noninteractive apt-get update
+
+DEBIAN_FRONTEND=noninteractive apt-get install -yq --force-yes \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+DEBIAN_FRONTEND=noninteractive add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+DEBIAN_FRONTEND=noninteractive apt-get update
+
+DEBIAN_FRONTEND=noninteractive apt-get install -yq --force-yes \
+    docker-ce
+
+
+git clone https://github.com/${SUDOWRT_REPO} ${SUDOWRT_DIR}
+
+
+crontab -l > new_cron
+
+echo "${CRON_TIME} * * * ${SUDOWRT_DIR}/auto_build" >> new_cron
+
+crontab new_cron
+rm new_cron
+
+# run initial build in background
+#nohup /opt/sudowrt-firmware/auto_build &>/dev/null &
+
+exit 0
