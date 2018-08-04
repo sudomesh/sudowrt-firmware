90ca9db4c977c1438e072dff9ba04750081896ff
paidforby <ggallo102@gmail.com>
2018-08-03 17:13:13 -0700

    rework build process related to #116 and #137

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-ARCH=ar71xx
-time ./build_only $ARCH
-#time ./build_extender-node $ARCH
+ARCH="ar71xx"
+TARGET="MYNETN600"
+BUILD_DIR=./built_firmware/builder.$ARCH
+
+# copy updated files from repo to build directory
+rm -r $BUILD_DIR/files
+cp -r ./files $BUILD_DIR/files
+
+# execute rebuild of firmware
+time ./build_only $ARCH $TARGET
 
 mkdir -p ./firmware_images
-cp -r ./built_firmware /firmware_images
+cp -r $BUILD_DIR/bin/ar71xx /firmware_images/.
