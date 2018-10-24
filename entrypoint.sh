0747f61b1d43a9ecf7768a75827517033b2bb538
paidforby <ggallo102@gmail.com>
2018-10-24 11:44:24 -0700

    simplify travis.yml, add date directory, add hash to banner

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@ ARCH="ar71xx"
 TARGET="MYNETN600"
 BUILD_DIR=./built_firmware/builder.$ARCH
 
+# add commit hash to banners
+GIT_COMMIT=$(git rev-parse HEAD)
+sed -i "s/<hash>/$GIT_COMMIT/g" ./files/etc/banner
+sed -i "s/<hash>/$GIT_COMMIT/g" ./files/opt/mesh/templates/etc/banner
+
 # copy updated files from repo to build directory
 rm -r $BUILD_DIR/files
 cp -r ./files $BUILD_DIR/files
